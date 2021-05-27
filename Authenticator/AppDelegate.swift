f3e359bb0232f5944b658831571d5f106ba15159
Bing Jeung <bing@ososo.io>
2021-05-27 09:35:13 +0800

    Don't call fatalError()

--- Authenticator/AppDelegate.swift
+++ Authenticator/AppDelegate.swift
@@ -46,7 +46,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                  * The store could not be migrated to the current model version.
                                  Check the error message to determine what the actual problem was.
                                  */
-                                fatalError("Unresolved error \(error), \(error.userInfo)")
+                                // fatalError("Unresolved error \(error), \(error.userInfo)")
+                                print(error)
                         }
                 })
                 return container
@@ -63,7 +64,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
                                 // Replace this implementation with code to handle the error appropriately.
                                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
                                 let nserror = error as NSError
-                                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                                // fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+                                print(nserror)
                         }
                 }
         }
