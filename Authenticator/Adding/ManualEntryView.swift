3f6cd043357fa88df46c1ec8360be82cf3b96e1d
Bing Jheung <bing@ososo.io>
2021-09-22 00:25:08 +0800

    Improve ManualEntryView

--- Authenticator/Adding/ManualEntryView.swift
+++ Authenticator/Adding/ManualEntryView.swift
@@ -1,147 +1,92 @@
 import SwiftUI
 
 struct ManualEntryView: View {
-        
+
         @Binding var isPresented: Bool
         let completion: (Token) -> Void
-        
+
         @State private var selection: Int = 0
         @State private var keyUri: String = ""
         @State private var issuer: String = ""
         @State private var accountName: String = ""
         @State private var secretKey: String = ""
-        
+
         @State private var isAlertPresented: Bool = false
-        
+
         var body: some View {
                 NavigationView {
-                        ZStack {
-                                GlobalBackgroundColor().ignoresSafeArea()
-                                ScrollView {
-                                        Picker("Method", selection: $selection) {
-                                                Text("By Key URI").tag(0)
-                                                Text("By Secret Key").tag(1)
-                                        }
-                                        .pickerStyle(SegmentedPickerStyle())
-                                        .padding()
-                                        
+                        VStack {
+                                Picker("Method", selection: $selection) {
+                                        Text("By Key URI").tag(0)
+                                        Text("By Secret Key").tag(1)
+                                }
+                                .pickerStyle(.segmented)
+                                .padding(.horizontal)
+                                List {
                                         if selection == 0 {
-                                                VStack {
-                                                        HStack {
-                                                                Text("Key URI")
-                                                                Spacer()
-                                                        }
-                                                        #if targetEnvironment(macCatalyst)
-                                                        TextField("otpauth://totp/...", text: $keyUri)
-                                                                .keyboardType(.URL)
-                                                                .disableAutocorrection(true)
-                                                                .autocapitalization(.none)
-                                                                .font(.system(.footnote, design: .monospaced))
-                                                                .textFieldStyle(RoundedBorderTextFieldStyle())
-                                                        #else
-                                                        EnhancedTextField(placeholder: "otpauth://totp/...",
-                                                                          text: $keyUri,
-                                                                          font: .monospacedSystemFont(ofSize: 13, weight: .regular),
-                                                                          keyboardType: .URL,
-                                                                          returnKey: .done,
-                                                                          autocorrection: .no,
-                                                                          autocapitalization: UITextAutocapitalizationType.none)
-                                                                .padding(10)
-                                                                .fillBackground(cornerRadius: 8)
-                                                        #endif
+                                                HStack {
+                                                        Text(verbatim: "Key URI")
+                                                        Spacer()
                                                 }
-                                                .padding()
+                                                .listRowSeparator(.hidden)
+                                                TextField("otpauth://totp/...", text: $keyUri)
+                                                        .keyboardType(.URL)
+                                                        .disableAutocorrection(true)
+                                                        .autocapitalization(.none)
+                                                        .font(.footnote.monospaced())
+                                                        .textFieldStyle(RoundedBorderTextFieldStyle())
                                         } else {
-                                                VStack {
-                                                        VStack {
-                                                                HStack {
-                                                                        Text("Issuer")
-                                                                        Spacer()
-                                                                }
-                                                                #if targetEnvironment(macCatalyst)
-                                                                TextField("Service Provider (Optional)", text: $issuer)
-                                                                        .disableAutocorrection(true)
-                                                                        .autocapitalization(.words)
-                                                                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                                                                #else
-                                                                EnhancedTextField(placeholder: NSLocalizedString("Service Provider (Optional)", comment: ""),
-                                                                                  text: $issuer,
-                                                                                  autocorrection: .no,
-                                                                                  autocapitalization: .words)
-                                                                        .padding(8)
-                                                                        .fillBackground(cornerRadius: 8)
-                                                                #endif
-                                                        }
-                                                        .padding()
-                                                        VStack {
-                                                                HStack {
-                                                                        Text("Account Name")
-                                                                        Spacer()
-                                                                }
-                                                                #if targetEnvironment(macCatalyst)
-                                                                TextField("email@example.com (Optional)", text: $accountName)
-                                                                        .keyboardType(.emailAddress)
-                                                                        .disableAutocorrection(true)
-                                                                        .autocapitalization(.none)
-                                                                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                                                                #else
-                                                                EnhancedTextField(placeholder: NSLocalizedString("email@example.com (Optional)", comment: ""),
-                                                                                  text: $accountName,
-                                                                                  keyboardType: .emailAddress,
-                                                                                  autocorrection: .no,
-                                                                                  autocapitalization: UITextAutocapitalizationType.none)
-                                                                        .padding(8)
-                                                                        .fillBackground(cornerRadius: 8)
-                                                                #endif
-                                                        }
-                                                        .padding(.horizontal)
-                                                        VStack {
-                                                                HStack {
-                                                                        Text("Secret Key")
-                                                                        Spacer()
-                                                                }
-                                                                #if targetEnvironment(macCatalyst)
-                                                                TextField("SECRET (Required)", text: $secretKey)
-                                                                        .keyboardType(.alphabet)
-                                                                        .disableAutocorrection(true)
-                                                                        .autocapitalization(.none)
-                                                                        .font(.system(.body, design: .monospaced))
-                                                                        .textFieldStyle(RoundedBorderTextFieldStyle())
-                                                                #else
-                                                                EnhancedTextField(placeholder: NSLocalizedString("SECRET (Required)", comment: ""),
-                                                                                  text: $secretKey,
-                                                                                  font: .monospacedSystemFont(ofSize: 17, weight: .regular),
-                                                                                  keyboardType: .alphabet,
-                                                                                  autocorrection: .no,
-                                                                                  autocapitalization: UITextAutocapitalizationType.none)
-                                                                        .padding(8)
-                                                                        .fillBackground(cornerRadius: 8)
-                                                                #endif
-                                                        }
-                                                        .padding()
+                                                HStack {
+                                                        Text("Issuer")
+                                                        Spacer()
                                                 }
+                                                .listRowSeparator(.hidden)
+                                                TextField("Service Provider (Optional)", text: $issuer)
+                                                        .disableAutocorrection(true)
+                                                        .autocapitalization(.words)
+                                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                                                HStack {
+                                                        Text("Account Name")
+                                                        Spacer()
+                                                }
+                                                .listRowSeparator(.hidden)
+                                                TextField("email@example.com (Optional)", text: $accountName)
+                                                        .keyboardType(.emailAddress)
+                                                        .disableAutocorrection(true)
+                                                        .autocapitalization(.none)
+                                                        .textFieldStyle(RoundedBorderTextFieldStyle())
+
+                                                HStack {
+                                                        Text("Secret Key")
+                                                        Spacer()
+                                                }
+                                                .listRowSeparator(.hidden)
+                                                TextField("SECRET (Required)", text: $secretKey)
+                                                        .keyboardType(.alphabet)
+                                                        .disableAutocorrection(true)
+                                                        .autocapitalization(.none)
+                                                        .font(.callout.monospaced())
+                                                        .textFieldStyle(RoundedBorderTextFieldStyle())
                                         }
                                 }
                         }
                         .alert(isPresented: $isAlertPresented) {
                                 Alert(title: Text("Error"), message: Text("Invalid Key"), dismissButton: .cancel(Text("OK")))
                         }
-                        .navigationTitle("Add account")
+                        .navigationTitle("Add Account")
+                        .navigationBarTitleDisplayMode(.inline)
                         .toolbar {
-                                ToolbarItem(placement: .navigationBarLeading) {
-                                        Button(action: {
-                                                isPresented = false
-                                        }) {
-                                                Text("Cancel")
-                                        }
+                                ToolbarItem(placement: .cancellationAction) {
+                                        Button("Cancel", action: { isPresented = false })
                                 }
-                                ToolbarItem(placement: .navigationBarTrailing) {
-                                        Button(action: handleAdding) { Text("Add") }
+                                ToolbarItem(placement: .confirmationAction) {
+                                        Button("Add", action: handleAdding)
                                 }
                         }
                 }
         }
-        
+
         private func handleAdding() {
                 var feedbackGenerator: UINotificationFeedbackGenerator? = UINotificationFeedbackGenerator()
                 if let token: Token = self.newToken {
@@ -155,7 +100,7 @@ struct ManualEntryView: View {
                         isAlertPresented = true
                 }
         }
-        
+
         private var newToken: Token? {
                 if selection == 0 {
                         guard !keyUri.isEmpty else { return nil }
