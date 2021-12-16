1e073370e78f1a47d396198764e7fab3beb9438f
Bing Zheung <bing@ososo.io>
2021-12-16 11:00:21 +0800

    Improve MenuCopyButton

--- Authenticator/ViewComponents/MenuCopyButton.swift
+++ Authenticator/ViewComponents/MenuCopyButton.swift
@@ -3,9 +3,9 @@ import SwiftUI
 struct MenuCopyButton: View {
         let content: String
         var body: some View {
-                Button(action: {
+                Button {
                         UIPasteboard.general.string = content
-                }) {
+                } label: {
                         Label("Copy", systemImage: "doc.on.doc")
                 }
         }
